9d82b0e409cd241fc367fe351e744b8fe6488ed6
Bro Wen <lour6498@gmail.com>
2019-02-11 15:54:39 +0800

    vue-sell skeleton

--- src/common/js/util.js
+++ src/common/js/util.js
@@ -18,4 +18,4 @@ export function urlParse() {
 		});
 	}
 	return obj;
-};
+}
---
+++ src/components/header/vheader.vue
@@ -0,0 +1,45 @@
+<template>
+
+  <div class="header">
+    
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+// import star from "./components/star/star"
+
+export default {
+  name: 'vheader',
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      detailShow: false
+    };
+  },
+  methods: {
+    showDetail() {
+      this.detailShow = true;
+    },
+    hideDetail() {
+      this.detailShow = false;
+    }
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+  },
+  components: {
+    // star
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "../../common/stylus/mixin";
+
+</style>
